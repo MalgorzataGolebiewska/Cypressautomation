b6320e30dc84082f99288cf7363539470a23905b
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-11-07 18:11:04 +0100

    File upload - Rename

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -2,10 +2,19 @@ import 'cypress-file-upload';
 
 describe('File Uploads',(()=>{
 
-    it.only('Single File Upload',()=>{
+    it('Single File Upload',()=>{
 
         cy.visit('https://the-internet.herokuapp.com/upload');
         cy.get('#file-upload').attachFile('intj.png');
+        cy.get('#file-submit').click();
+        cy.wait(5000);
+        cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+    })
+
+    it.only('File Upload - Rename',()=>{
+
+        cy.visit('https://the-internet.herokuapp.com/upload');
+        cy.get('#file-upload').attachFile({filePath:'intj.png', fileName:'myFile.png'});
         cy.get('#file-submit').click();
         cy.wait(5000);
         cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
