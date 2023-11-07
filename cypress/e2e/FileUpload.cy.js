445fd6baed0ef08a3fe76d62d9691445a81063be
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-11-07 18:56:26 +0100

    File Upload - Drag and drop

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -11,12 +11,20 @@ describe('File Uploads',(()=>{
         cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
     })
 
-    it.only('File Upload - Rename',()=>{
+    it('File Upload - Rename',()=>{
 
         cy.visit('https://the-internet.herokuapp.com/upload');
         cy.get('#file-upload').attachFile({filePath:'intj.png', fileName:'myFile.png'});
         cy.get('#file-submit').click();
         cy.wait(5000);
         cy.get("div[class='example'] h3").should('have.text','File Uploaded!');
+    })
+
+     it.only('File Upload - Drag and drop',()=>{
+
+        cy.visit('https://the-internet.herokuapp.com/upload');
+        cy.get('#drag-drop-upload').attachFile('intj.png', {subjectType:'drag-n-drop'});
+        cy.wait(5000);
+        cy.get('#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span').contains('intj.png');
     })
 }))
