b00c0d7ad24d02fcfc8c426e09c1d339c2bc0012
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-11-08 18:25:11 +0100

    Fixtures Test with before() hook

--- cypress/e2e/Fixtures.cy.js
+++ cypress/e2e/Fixtures.cy.js
@@ -1,17 +1,37 @@
 describe('MyTestSuite', ()=>{
 
-    it.only('FixtureDemoTest', ()=> {
+    //Direct access
+    // it.only('FixtureDemoTest', ()=> {
+
+    // cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    // cy.fixture('orangehrm.json').then( (data)=>{
+
+    //     cy.get("input[placeholder='Username']").type(data.username);
+    //     cy.get("input[placeholder='Password']").type(data.password);
+    //     cy.get("button[type='submit']").click();
+
+    //     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text',data.expected);
+    // })
+// })
+
+let userdata;
+before( ()=>{
+    cy.fixture('orangehrm').then((data)=>{
+        userdata=data;
+    })
+})
+
+it.only('FixtureDemoTest', ()=> {
 
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    cy.fixture('orangehrm.json').then( (data)=>{
-
-        cy.get("input[placeholder='Username']").type(data.username);
-        cy.get("input[placeholder='Password']").type(data.password);
+        cy.get("input[placeholder='Username']").type(userdata.username);
+        cy.get("input[placeholder='Password']").type(userdata.password);
         cy.get("button[type='submit']").click();
 
-        cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text',data.expected);
-    })
+        cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text',userdata.expected);
+
 })
 
 })
