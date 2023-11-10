cb0cc3dfc9f025cc2370cc305b3b74ec874bef51
MalgorzataGolebiewska <malgorzata.golebiewska01@gmail.com>
2023-11-10 18:25:16 +0100

    Custom Command - overwriting existing command

--- cypress/e2e/CustomCommand.cy.js
+++ cypress/e2e/CustomCommand.cy.js
@@ -1,10 +1,18 @@
 describe('Custom commands', () =>{
 
-    it.only("handling links", () =>{
+    it("handling links", () =>{
 
         cy.visit("https://demo.nopcommerce.com/");
 
         cy.clickLink("Apple MacBook Pro 13-inch");
+        cy.get("div[class='product-name'] h1").should('have.text','Apple MacBook Pro 13-inch');
+    })
+
+    it.only("overwriting existing command", () =>{
+
+        cy.visit("https://demo.nopcommerce.com/");
+
+        cy.clickLink("APPLE MACBOOK Pro 13-inch");
         cy.get("div[class='product-name'] h1").should('have.text','Apple MacBook Pro 13-inch');
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,6 +28,8 @@
 
 ///<reference types="cypress-xpath" />
 
+
+
 Cypress.Commands.add('getIframe',(iframe)=>{
     return cy.get(iframe)
             .its('0.contentDocument.body')
@@ -39,3 +41,30 @@ Cypress.Commands.add('getIframe',(iframe)=>{
 Cypress.Commands.add('clickLink',(label)=>{
     cy.get('a').contains(label).click();
 })
+
+Cypress.Commands.overwriteQuery(
+    "contains",
+    function (contains, filter, text, userOptions = {}) {
+  
+      // This is parameter resolution from Cypress v12.7.0 source
+      if (Cypress._.isRegExp(text)) {
+        // .contains(filter, text)
+        // Do nothing
+      } else if (Cypress._.isObject(text)) {
+        // .contains(text, userOptions)
+        userOptions = text
+        text = filter
+        filter = ''
+      } else if (Cypress._.isUndefined(text)) {
+        // .contains(text)
+        text = filter
+        filter = ''
+      }
+  
+      userOptions.matchCase = false;
+  
+      let contains0 = contains.bind(this)    // this line fixes the error
+  
+      return contains0(filter, text, userOptions)
+    }
+  )
